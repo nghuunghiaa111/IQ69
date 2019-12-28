4eee5672901b93ca8e70035db00b8f93e623a409
Nghia Nguyen Huu <nghuunghiaa@gmail.com>
2019-12-28 10:36:48 +0700

    Initial Commit

---
+++ IQ69/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IQ69
+//
+//  Created by HuuNghia on 12/28/19.
+//  Copyright © 2019 HuuNghia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
